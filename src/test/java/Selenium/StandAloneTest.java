864cec12425b98e0af14287a9bb8f5bfef72bd55
Maruf_Monem <marufmonem@gmail.com>
2022-09-28 12:16:11 +0600

    Successfully logged in

--- src/test/java/Selenium/StandAloneTest.java
+++ src/test/java/Selenium/StandAloneTest.java
@@ -1,15 +1,25 @@
 package Selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+import java.time.Duration;
 
 public class StandAloneTest {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit wait
+        driver.get("https://rahulshettyacademy.com/client/");
+//      User name
+        driver.findElement(By.id("userEmail")).sendKeys("maruftest@yahoo.com");
+//        Password
+        driver.findElement(By.id("userPassword")).sendKeys("Test123456");
+//        Login
+        driver.findElement(By.id("login")).click();
+
     }
 
 }
