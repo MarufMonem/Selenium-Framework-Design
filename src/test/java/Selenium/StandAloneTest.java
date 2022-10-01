e7ed6c96e6a813a26cfffdb1dcd20a03a116f868
Maruf_Monem <marufmonem@gmail.com>
2022-10-01 09:48:18 +0600

    Added wait for the animation and toast message. Added code for cart button.

--- src/test/java/Selenium/StandAloneTest.java
+++ src/test/java/Selenium/StandAloneTest.java
@@ -2,15 +2,20 @@ package Selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
 
 public class StandAloneTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//        Initial setup
+        String desiredProduct = "Zara";
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit wait
@@ -21,13 +26,13 @@ public class StandAloneTest {
         driver.findElement(By.id("userPassword")).sendKeys("Test123456");
 //        Login
         driver.findElement(By.id("login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+//        Waiting until all the products are visible
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mb-3")));
+
 //        All products
         List<WebElement> allProdcuts = driver.findElements(By.className("mb-3"));
-
-//        for (WebElement product: allProdcuts) {
-//            String name = product.findElement(By.tagName("b")).getText();
-//            System.out.println(name);
-//        }
 
 //        Here we are filtering out the product that has the name ZARA in it. If there are multiple
 //        elements having the word ZARA then return only one.
@@ -38,6 +43,13 @@ public class StandAloneTest {
                         .getText().contains("ZARA")).findFirst().orElse(null);
         desiredItem.findElement(By.cssSelector("button:last-of-type")).click();
 
+//        We need to wait for the animation to be over and toast message to appear
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='toast-container']")));
+
+        driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
+        Thread.sleep(10000);
+        driver.quit();
     }
 
 }
