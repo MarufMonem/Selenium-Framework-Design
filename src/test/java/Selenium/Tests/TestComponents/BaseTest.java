2f99783f5edcd7a9cb5dc0eaace310e368890d79
Maruf_Monem <marufmonem@gmail.com>
2022-10-16 16:44:32 +0600

    BaseTest.java can now handle chrome setup

--- src/test/java/Selenium/Tests/TestComponents/BaseTest.java
+++ src/test/java/Selenium/Tests/TestComponents/BaseTest.java
@@ -4,18 +4,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 
 public class BaseTest {
+    public WebDriver driver;
 
-    public void intializeDriver(){
-
+    public void intializeDriver() throws IOException {
 //        Properties class
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\Selenium\\Resources\\GlobalData.properties");
+        prop.load(fis);
+       String browserName =  prop.getProperty("browser");
 
 //        Chrome
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        if(browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        }else{
+            System.out.println("we dont have this");
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit wait
         driver.manage().window().maximize();
+    }
+
+    public void launchApplication(){
+
     }
 }
