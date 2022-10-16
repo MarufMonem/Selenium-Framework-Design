df409ebc9bc4d8e212d67989020d467249044dbc
Maruf_Monem <marufmonem@gmail.com>
2022-10-16 18:05:31 +0600

    Error validation for login failure implemented

--- src/test/java/Selenium/Tests/ModularizedTest.java
+++ src/test/java/Selenium/Tests/ModularizedTest.java
@@ -1,25 +1,15 @@
 package Selenium.Tests;
 
-import Selenium.Tests.TestComponents.BaseTest;
+import Selenium.TestComponents.BaseTest;
 import Selenium.pageObjectModel.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.checkerframework.checker.units.qual.C;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.List;
 
-public class StandAloneTest_Test extends BaseTest {
+public class ModularizedTest extends BaseTest {
 
     @Test
     public  void submitOrder() throws InterruptedException, IOException {
